83943d973cb447954d2ec9c638903ee09b9fcb3e
PimplePrincess <katerina.teghai@bk.ru>
2025-07-17 16:50:15 +0300

    Проверка версии питона

---
+++ 2_PythonVersionChecker.py
@@ -0,0 +1,10 @@
+import sys
+def main():
+    print("Version of Python:")
+    print(sys.version)
+    print("Version info:")
+    print(sys.version_info)
+
+if __name__ == '__main__':
+    main()
+
