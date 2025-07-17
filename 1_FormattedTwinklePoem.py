e5abc1fe7cd8b18d68b9a4c17a85cd2771f2c981
PimplePrincess <katerina.teghai@bk.ru>
2025-07-17 16:45:17 +0300

    Отформатированная песня про звёздочку

---
+++ 1_FormattedTwinklePoem.py
@@ -0,0 +1,11 @@
+def main():
+    print("Twinkle, twinkle, little star,")
+    print("\t How I wonder what you are!")
+    print("\t\t Up above the world so high,")
+    print("\t\t Like a diamond in the sky.")
+    print("Twinkle, twinkle, little star,")
+    print("\t How I wonder what you are!")
+
+if __name__ == '__main__':
+    main()
+
